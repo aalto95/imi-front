c5ec7b0cdada3829782daf929c9093d5b42f9a42
y2k01 <scotlandfjord@protonmail.com>
2022-03-17 21:26:49 +0900

    Add tailwind color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   theme: {
     colors: {
       'terracota': '#943813',
+      'berlin-blue': '#003153',
+      'carousel-arrows': '#c7ccd2',
+      'antique-white': '#faebd7',
+      'light-gray': '#ebf3f8'
     },
     extend: {
     },
