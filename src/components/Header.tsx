0f4a9b418c8878adc28a9b62c34494323a34a28e
y2k01 <scotlandfjord@protonmail.com>
2022-04-30 17:56:49 +0900

    Fix styles in navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,6 @@ export const Header = () => {
   const [isSidebarActive, setIsSidebarActive] = useRecoilState(sidebarState)
   const onHamburgerClick = () => {
     setIsSidebarActive(!isSidebarActive)
-    console.log(isSidebarActive)
   }
   const [navigation] = useState([
     {
@@ -42,17 +41,17 @@ export const Header = () => {
     }
   ])
   return (
-    <header className="mx-4 xl:mx-24 2xl:mx-32">
+    <header className="mx-12 xl:mx-24 2xl:mx-32 mb-4">
       <div className="flex justify-between my-12">
         <Link to="/" className="flex items-center">
           <img src={logo} alt="imi-logo" className="w-10 md:w-16 lg:w-24 mr-4" />
-          <h1 className="text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl w-72 text-berlin-blue">ИНСТИТУТ МАТЕМАТИКИ И ИНФОРМАТИКИ</h1>
+          <h1 className="text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl text-berlin-blue">ИНСТИТУТ МАТЕМАТИКИ<br/> И ИНФОРМАТИКИ</h1>
         </Link>
-        <div className="hidden lg:flex lg:items-center">
+        <div className="hidden md:flex md:items-center">
           <Link to="/" className="mr-4 text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl">Контакты</Link>
           <a href="tel:+74112496834" className="text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl">+7 (4112) 49-68-34</a>
         </div>
-        <div className="flex items-center lg:hidden">
+        <div className="flex items-center md:hidden">
           <label htmlFor="check">
             <input type="checkbox" id="check" onClick={() => onHamburgerClick()}/> 
             <span></span>
@@ -61,10 +60,10 @@ export const Header = () => {
           </label>
         </div>
       </div>
-      <nav className="hidden lg:visible lg:flex lg:justify-between">
+      <nav className="hidden md:visible md:flex md:justify-between">
         {
           navigation.map((navElem) => (
-          <Link to={navElem.path} className="text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl uppercase text-terracota" key={navElem.name}>{navElem.name}</Link>
+          <Link to={navElem.path} className="md:text-xs lg:text-sm xl:text-md 2xl:text-lg uppercase text-terracota" key={navElem.name}>{navElem.name}</Link>
           ))
         }
       </nav>
