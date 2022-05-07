6d3e74611b484c24e246caf55890aaeebc41df8e
y2k01 <scotlandfjord@protonmail.com>
2022-05-07 17:41:20 +0900

    Add date showing in news banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
 import './App.css';
@@ -7,9 +7,15 @@ import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import MainPage from './pages/MainPage'
 import { isSidebarActiveState } from './recoil/selectors/sidebarSelector'
+import { enGB, eo, ru } from 'date-fns/locale'
+
+declare const window: any;
 
 const App = () => {
   const isSidebarActive = useRecoilValue(isSidebarActiveState)
+  useEffect(() => {
+    window.__localeId__ = 'ru'
+  }, []);
   return (
     <div>
       <Header />
--- src/components/NewsSlider.tsx
+++ src/components/NewsSlider.tsx
@@ -1,5 +1,8 @@
+
+import { format, getMonth } from 'date-fns'
 import { FunctionComponent, useState } from 'react'
 import { Slide } from '../models/slide'
+import { ru } from 'date-fns/locale'
 
 interface NewsSliderProps {
   slides: Slide[]
@@ -8,8 +11,8 @@ interface NewsSliderProps {
 const NewsSlider: FunctionComponent<NewsSliderProps> = ({slides}) => {
   const [firstSlide, setFirstSlide] = useState(0)
   return (
-    <div>
-      <div className="flex justify-between mx-4 sm:mx-8 md:mx-12 mt-8 mb-4">
+    <div className="my-8">
+      <div className="flex justify-between mx-4 sm:mx-8 md:mx-12 mb-4">
         <h1 className="text-terracota">НОВОСТИ ИМИ</h1>
         <p className="text-black">Все новости</p>
       </div>
@@ -19,16 +22,26 @@ const NewsSlider: FunctionComponent<NewsSliderProps> = ({slides}) => {
         </div>
         <div className='hidden xs:block w-1/2 md:w-1/3'>
           <img src={slides[firstSlide + 1].image} alt="" className="h-48 w-full object-cover"/>
-          <div className="p-4">
-            <h3 className="font-bold">{slides[firstSlide + 1].label}</h3>
-            <p className="text-black">{slides[firstSlide + 1].paragraph}</p>
+          <div className="p-4 flex flex-col justify-between">
+            <div>
+              <h3 className="font-bold">{slides[firstSlide + 1].label}</h3>
+              <p className="text-black">{slides[firstSlide + 1].paragraph}</p>
+            </div>
+            <div className="flex justify-end">
+              <p className="text-black">{format(new Date(), 'dd/MM/yyyy')}</p>
+            </div>
           </div>
         </div>
         <div className='w-1/3 hidden md:block'>
           <img src={slides[firstSlide + 2].image} alt="" className="h-48 w-full object-cover"/>
-          <div className="p-4">
-            <h3 className="font-bold">{slides[firstSlide + 2].label}</h3>
-            <p className="text-black">{slides[firstSlide + 2].paragraph}</p>
+          <div className="p-4 flex flex-col justify-between">
+            <div>
+              <h3 className="font-bold">{slides[firstSlide + 2].label}</h3>
+              <p className="text-black">{slides[firstSlide + 2].paragraph}</p>
+            </div>
+            <div className="flex justify-end">
+                <p className="text-black">{format(new Date(), 'dd/MM/yyyy')}</p>
+            </div>
           </div>
         </div>
       </div>
